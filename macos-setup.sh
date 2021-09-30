4dcb1b98b2fa18abf7105534ce340209cca24c85
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2021-09-30 19:15:22 +0530

    Update macos-setup.sh

--- macos-setup.sh
+++ macos-setup.sh
@@ -1,1 +1,5 @@
-testing something
+#!/usr/bin/env bash
+# curl https://piyush1104.github.io/macos-setup.sh | bash
+# /bin/bash -c "$(curl -fsSL https://piyush1104.github.io/macos-setup.sh)"
+
+echo "Hello $(whoami)! Let's get you set up."
