7fe132ab81ed872d114e540c302e86ae16e918b6
Piyush Bansal <19729997+piyush1104@users.noreply.github.com>
2025-05-27 23:17:06 +0530

    add pre config changes for nvim

--- macos-setup.sh
+++ macos-setup.sh
@@ -715,6 +715,10 @@ setup_vim_tools() {
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     echo "Installed plug.vim, remember to do :PlugInstall after opening vim"
 
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    echo "Installed plug.vim for nvim. Do :PlugInstall after opening nvim"
+
 }
 
 setup_vim_tools
